b6965f7d8c0f71bd90790f68d8681d5a6b0af777
Kerim Demirel <60410593+kdemirel@users.noreply.github.com>
2022-09-30 18:03:35 +0300

    old versions are removed

--- Tureng 6.0.2/content_script.js
+++
@@ -1,9 +0,0 @@
-chrome.extension.onRequest.addListener(
-	function(request, sender, callBack) {
-		if (request.action == 'get_selected_text'){
-			callBack({selected_text: window.getSelection().toString()});
-        }else{
-			callBack(null);
-        }
-	}
-);
